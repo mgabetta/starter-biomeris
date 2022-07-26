768fee5ffee3eca58198068a80108429a35202fc
matteogabetta <matteo.gabetta@gmail.com>
2022-07-26 11:29:55 +0200

    aggiornamento LUG22

--- SH/s3_get_R_run.sh
+++ SH/s3_get_R_run.sh
@@ -1,56 +1,69 @@
-#TODO decide if more than 1 file to be executed can be on the remote repo 
-URL="https://biomeris-int-vantage-test.s3-eu-west-1.amazonaws.com/exec_1.R"
-script_wd="/opt/redcap_dq/environment/scripts/"
-logs="/opt/redcap_dq/environment/logs/"
+#TODO decide if more than 1 file to be executed can be on the remote repo
+LOCK_FILE="/opt/redcap_dq/environment/PROCESS_LOCK_FILE"
 general_log="/opt/redcap_dq/environment/logs/log.txt"
 
-today=$(date +"%y%m%d_%H%M%S")
+if ! [ -f "$LOCK_FILE" ]; then
+	URL="https://biomeris-int-vantage-test.s3-eu-west-1.amazonaws.com/exec_1.R"
+	script_wd="/opt/redcap_dq/environment/scripts/"
+	logs="/opt/redcap_dq/environment/logs/"
 
-log_folder="$logs""$today" 
+	touch $LOCK_FILE
 
-if ! [ -d "$log_folder" ]; then
-	mkdir "$log_folder";
+	# these variable are safe to be set only if the process isn't locked
+	# otherwise they affect the previous execution that is not finished
+	today=$(date +"%y%m%d_%H%M%S")
+	log_folder="$logs""$today" 
 
-	#log.txt
-	echo -n  $(date +"%x-%X") >> $general_log
-	echo " folder $log_folder created" >> $general_log
-fi
 
-if wget -T 20 -q "$URL" -P "$script_wd" > /dev/null; then
+	if ! [ -d "$log_folder" ]; then
+		mkdir "$log_folder";
 
-	#log.txt
-	echo -n  $(date +"%x-%X") >> $general_log
-	echo " file exec_1.R downloaded from $URL" >> $general_log
+		#log.txt
+		echo -n  $(date +"%x-%X") >> $general_log
+		echo " folder $log_folder created" >> $general_log
+	fi
+
+	if wget -T 20 -q "$URL" -P "$script_wd" > /dev/null; then
+
+		#log.txt
+		echo -n  $(date +"%x-%X") >> $general_log
+		echo " file exec_1.R downloaded from $URL" >> $general_log
+		
+		#log.txt
+		echo -n  $(date +"%x-%X") >> $general_log
+		echo " file exec_1.R moved to $script_wd" >> $general_log 
+
+		cd "$script_wd"
+		touch Rlog.txt
+		Rscript exec_1.R > "Rlog.txt" 2>&1
+		
+		#log.txt
+		echo -n  $(date +"%x-%X") >> $general_log
+		echo " script exec_1.R run with exit code $?" >> $general_log
+		
+		mv ./* "$log_folder"
+		
+		#log.txt
+		echo -n  $(date +"%x-%X") >> $general_log
+		echo " file exec_1.R and all generated resources moved to $log_folder" >> $general_log
+		
+	else
+		#log.txt
+		echo -n  $(date +"%x-%X") >> $general_log
+		echo " impossible to download file exec_1.R" >> $general_log
+	fi
+
+	# even if there are errors this part of the script will be executed so 
+	# the lock file will be removed only in the execution in which it is 
+	# created
+	rm $LOCK_FILE
 	
-	#log.txt
-	echo -n  $(date +"%x-%X") >> $general_log
-	echo " file exec_1.R moved to $script_wd" >> $general_log 
-
-	cd "$script_wd"
-	touch Rlog.txt
-	sudo Rscript exec_1.R > "Rlog.txt" 2>&1
-	
-	#log.txt
-	echo -n  $(date +"%x-%X") >> $general_log
-	echo " script exec_1.R run with exit code $?" >> $general_log
-	
-	mv ./* "$log_folder"
-	
-	#log.txt
-	echo -n  $(date +"%x-%X") >> $general_log
-	echo " file exec_1.R and all generated resources moved to $log_folder" >> $general_log
-	
+	unset log_folder
+	unset today
+	unset logs
+	unset script_wd
+	unset URL
 else
-	#log.txt
-	echo -n  $(date +"%x-%X") >> $general_log
-	echo " impossible to download file exec_1.R" >> $general_log
+		echo -n  $(date +"%x-%X") >> $general_log
+		echo " SKIPPED - Waiting for previous execution to finish." >> $general_log
 fi
-
-
-
-unset log_folder
-unset today
-unset general_log
-unset logs
-unset script_wd
-unset URL
